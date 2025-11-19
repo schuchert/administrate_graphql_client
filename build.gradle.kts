10652951ce3737630c6f3a7b4b03f65ab182569a
Brett Schuchert <schuchert@yahoo.com>
2025-11-19 09:33:04 -0600

    first attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.7"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.graphql-java-generator.graphql-gradle-plugin3") version "3.0.1"
 }
 
 group = "com.fsi.graphql"
@@ -27,6 +28,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("com.graphql-java-generator:graphql-java-client-runtime:3.0.1")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
@@ -36,4 +38,14 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+// GraphQL Java Generator configuration
+afterEvaluate {
+	extensions.configure<Any>("graphql") {
+		val groovyObj = this as groovy.lang.GroovyObject
+		groovyObj.setProperty("schemaFileFolder", "src/main/resources")
+		groovyObj.setProperty("schemaFilePattern", "schema.graphql")
+		groovyObj.setProperty("packageName", "com.fsi.graphql.client.generation.generated")
+	}
 }
